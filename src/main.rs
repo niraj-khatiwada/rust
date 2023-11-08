8f82de9e8ce9d122ab33349b49d9a65300e3d7ba
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-08 17:34:49 +0545

    reader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-fn main() {}
+use std::io;
+
+fn main() {
+    println!("Enter your name");
+    let mut name: String = String::new();
+    // match io::stdin().read_line(&mut name) {
+    //     Ok(size) => println!("{:?}", size),
+    //     Err(err) => println!("{:?}", err)
+    // }
+
+    if let Ok(_) = io::stdin().read_line(&mut name) {
+        println!("Your name is {:?}", name.trim());
+    } else {
+        println!("Error occurred")
+    }
+}
 
 
 
--- src/reader.rs
+++ src/reader.rs
@@ -1,10 +1,35 @@
+// fn main() {
+//     reader();
+// }
+//
+// fn reader() {
+//     println!("What is your name?");
+//     let mut name: String = String::new();
+//     io::stdin().read_line(&mut name).expect("Oops!");
+//     println!("Hello {}", name.trim());
+// }
+
+use std::io;
+
 fn main() {
-    reader();
+    println!("Enter your name");
+    let mut name: String = String::new();
+    // match io::stdin().read_line(&mut name) {
+    //     Ok(size) => println!("{:?}", size),
+    //     Err(err) => println!("{:?}", err)
+    // }
+
+    if let Ok(_) = io::stdin().read_line(&mut name) {
+        println!("Your name is {:?}", name.trim());
+    } else {
+        println!("Error occurred")
+    }
 }
 
-fn reader() {
-    println!("What is your name?");
-    let mut name: String = String::new();
-    io::stdin().read_line(&mut name).expect("Oops!");
-    println!("Hello {}", name.trim());
-}
+
+
+
+
+
+
+
