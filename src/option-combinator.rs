b2fccce9b64897e1f5b36ea1119a6a50676d33d3
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-08 13:21:23 +0545

    iterators

--- src/option-combinator.rs
+++ src/option-combinator.rs
@@ -16,6 +16,22 @@ fn main() {
         println!("Value is {:?}", value);
         return value * 2;
     }); // You can chain as many map as you want
+    println!("{}", maybe_number_doubled.is_some()); // true when Some<> is returned
+    println!("{}", maybe_number_doubled.is_none()); // true when None is returned
+
+    // Filter combinator
+    // Filter out the returned Some value based on certain condition. If returned false, it will remove the Some<> and return None instead.
+
+    let filter = maybe_number().filter(|value| value < &50);
+
+    println!("{}", filter.is_some()); // true when Some<> is returned
+    println!("{}", filter.is_none()); // true when None is returned
+
+    let or_else = maybe_number().or_else(|| Some(100)); // If maybe_number has Some<> return that otherwise return Some(100). Used for setting default value.
+    let unwrap_or_else = maybe_number().unwrap_or_else(|| 100); // Same as or_else but or_else needs to return Option<T> type where as this needs to return T.
+
+    dbg!(or_else);
+    dbg!(unwrap_or_else);
 
     match maybe_number_doubled {
         Some(value) => println!("Doubled value is {:?}", value),
@@ -23,6 +39,7 @@ fn main() {
     }
 }
 
+// Start from 72
 
 
 
