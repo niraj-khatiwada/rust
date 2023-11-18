f2eb2d3324e4ea8b44a8ec3d34bb2788239aa96c
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-18 22:10:58 +0545

    iterator and intoterator trait

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-#[derive(PartialEq, PartialOrd)]
-enum Status {
-    // 0
-    Active,
-    // 1
-    Deleted,
-    // Archived
-    Archived(i32),
-}
-
-fn main() {
-    let abc = Status::Active;
-
-    println!("{:?}", abc == Status::Active);
-    println!("{:?}", Status::Active < Status::Deleted); // Order
-    println!("{:?}", Status::Archived(99) < Status::Archived(100));
-}
+fn main() {}
--- src/partial-equality.rs
+++ src/partial-equality.rs
@@ -8,10 +8,21 @@ enum Status {
     Archived(i32),
 }
 
+#[derive(PartialEq, PartialOrd)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
 fn main() {
     let abc = Status::Active;
 
     println!("{:?}", abc == Status::Active);
     println!("{:?}", Status::Active < Status::Deleted); // Order
     println!("{:?}", Status::Archived(99) < Status::Archived(100));
+
+    let person1 = Person { name: String::from("niraj"), age: 26 };
+    let person2 = Person { name: String::from("niraj"), age: 27 };
+
+    println!("{:?}", person1 == person2)
 }
