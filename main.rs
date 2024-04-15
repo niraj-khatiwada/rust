9d62930f0ef426ca9941b6e0a93c11edb272e42e
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-04-15 21:28:48 +0545

    closures, hashmaps

--- main.rs
+++ main.rs
@@ -1,17 +1,1 @@
-use rand::Rng;
-
-fn main() {
-    println!("{:?}", get_name());
-}
-
-/// Get's name randomly
-fn get_name() -> Result<String, String> {
-    let mut thread_rng = rand::thread_rng();
-    let random: u32 = thread_rng.gen();
-
-    if random % 2 == 0 {
-        Ok(String::from("Niraj"))
-    } else {
-        Err(String::from("Name is missing."))
-    }
-}
+fn main() {}
---
+++ src/closure.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let sum = |a: i32, b: i32| a + b;
+    println!("{}", sum(1, 2));
+}
+
+// fn sum(a: i32, b: i32) -> i32 {
+//     a + b
+// }
