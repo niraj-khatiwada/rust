e52b4970e4ab502b20edb171168e2c4261d83b5b
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-01-09 21:45:02 +0545

    vector

--- main.rs
+++ main.rs
@@ -1,24 +1,12 @@
+enum Status {
+    Active(u32),
+    Inactive,
+}
+
 fn main() {
-    let mut list: Vec<String> = vec![String::from("Rust"), String::from("Golang")];
-    list.push(String::from("Zig"));
+    let activity_status = Status::Active(100);
 
-    for itm in &list {
-        println!("{}", itm);
+    if let Status::Active(val) = activity_status {
+        println!("{}", val)
     }
-
-    // Map: Map means we need to map to new owned value. So, the closure returns &String
-    let mapped: Vec<String> = list
-        .iter()
-        .map(|lan: &String| {
-            if lan.eq("Rust") {
-                return String::from("Rust is King");
-            }
-            String::from("Boring")
-        })
-        .collect();
-    dbg!(mapped);
-
-    // Filter: Filter means to filter out items based on condition. Since, the original vector is Vec<String> it can only return reference items as Vec<&String>. That's why the closure returns &&String
-    let filter: Vec<&String> = list.iter().filter(|lan| (*lan).eq("Rust")).collect();
-    dbg!(filter);
 }
