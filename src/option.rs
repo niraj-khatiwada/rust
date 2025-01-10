6e7a5e343b17b42e9b2bf95a18ba656bc9e670f9
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-01-10 13:30:14 +0545

    Option & Result combinators

--- src/option.rs
+++ src/option.rs
@@ -4,11 +4,38 @@ fn main() {
         println!("Found: {}", val);
     }
 
-    // Map combinator to transform option value
-    let transform = search.map(|val| format!("{}!!!", val));
-    if let Some(val) = &transform {
-        println!("Transform: {}", val);
+    println!("Is Some?: {}", search.is_some());
+    println!("Is None?: {}", search.is_none());
+
+    // Differences in & and .as_ref(). We can also use .as_mut() for mutable reference.
+    let _ref = &search; // Option<String> -> &Option<String>
+    let _ref = search.as_ref(); // Option<String> -> Option<&String>
+
+    // Map: Map the Some(val) to another value.
+    let map = search.as_ref().map(|val| format!("{}!!!", val));
+    if let Some(val) = map {
+        println!("Map: {}", val);
     }
+
+    // Filter: Filter out if the Some(val) has some specific data.
+    let filter = search.as_ref().filter(|val| val == &"Rust is king");
+    if let Some(val) = &filter {
+        println!("Filter: {}", val);
+    }
+
+    // or_else: Return the actual Some(val) or return new Some(default) when None is found.
+    let default_val = String::from("Rust will come back.");
+    let or_else = search.as_ref().or_else(|| Some(&default_val));
+    if let Some(val) = &or_else {
+        println!("Or else: {}", val);
+    }
+
+    // unwrap_or_else: Same as or_else but the value will be unwrapped from Some(..) and directly return the content.
+    let or_else = search.as_ref().unwrap_or_else(|| &default_val);
+
+    // We can also avoid the closures by directly using or and unwrap_or.
+    let or = search.as_ref().or(Some(&default_val));
+    let unwrap_or = search.as_ref().unwrap_or(&default_val);
 }
 
 /// Finds a value based on the search term. If not found, returns None.
--- src/result.rs
+++ src/result.rs
@@ -4,9 +4,6 @@ fn main() {
         Ok(rs) => println!("Found: {}", rs),
         Err(err) => println!("Error: {}", err),
     }
-    // Map combinator
-    let transform = search2.map(|rs| format!("{}!!!", rs));
-    println!("{}", transform.unwrap());
 }
 
 /// Finds a value based on the search term. If not found, throws error.
@@ -21,4 +18,47 @@ fn find_row_or_throw(name: &str) -> Result<String, String> {
 fn find_row_again_or_throw(name: &str) -> Result<String, String> {
     let rs = find_row_or_throw(name)?;
     Ok(rs)
+}
+
+// Result Combinators
+fn main() {
+    let search = find_row("Rust");
+    if let Ok(val) = &search {
+        println!("Found: {}", val);
+    }
+
+    println!("Is Ok?: {}", search.is_ok());
+    println!("Is Err?: {}", search.is_err());
+
+    // Differences in & and .as_ref(). We can also use .as_mut() for mutable reference.
+    let _ref = &search; // Result<String, ()> -> &Result<String, ()>
+    let _ref = search.as_ref(); // Result<String, ()> -> Result<&String, ()>
+
+    // Map: Map the Ok(val) to another value.
+    let map = search.as_ref().map(|val| format!("{}!!!", val));
+    if let Ok(val) = map {
+        println!("Map: {}", val);
+    }
+
+    // or_else: Return the actual Some(val) or return new Ok(default) when Err is found.
+    let default_val = String::from("Rust will come back.");
+    let or_else: Result<&String, ()> = search.as_ref().or_else(|err| Ok(&default_val));
+    if let Ok(val) = &or_else {
+        println!("Or else: {}", val);
+    }
+
+    // unwrap_or_else: Same as or_else but the value will be unwrapped from Ok(..) and directly return the content.
+    let or_else = search.as_ref().unwrap_or_else(|err| &default_val);
+
+    // We can also avoid the closures by directly using or and unwrap_or.
+    let or: Result<&String, ()> = search.as_ref().or(Ok(&default_val));
+    let unwrap_or = search.as_ref().unwrap_or(&default_val);
+}
+
+/// Finds a value based on the search term. If not found, returns None.
+fn find_row(name: &str) -> Result<String, ()> {
+    if name == "Rust" {
+        return Ok(String::from("Rust is king"));
+    }
+    Err(())
 }
