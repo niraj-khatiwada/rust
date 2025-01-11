442ef5e43df9ffbd4f1453952be97e92a6ed30d1
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-01-11 21:55:27 +0545

    custom error

--- main.rs
+++ main.rs
@@ -1,1 +1,29 @@
-fn main() {}
+use thiserror::Error;
+
+struct Package {
+    name: String,
+    is_shipped: bool,
+    error: Option<PackageError>,
+}
+
+#[derive(Debug, Error)]
+enum PackageError {
+    #[error("Package not found. Reason=\"{reason}\"")]
+    NotFound { reason: String },
+    #[error("Package is damaged.")]
+    Damaged,
+    #[error("Package is lost.")]
+    Lost,
+}
+
+fn main() {
+    let package = Package {
+        name: String::from("Laptop"),
+        is_shipped: false,
+        error: Some(PackageError::NotFound {
+            reason: String::from("Location mismatch."),
+        }),
+    };
+
+    println!("{}", package.error.unwrap()); // Package not found. Reason="Location not found"
+}
