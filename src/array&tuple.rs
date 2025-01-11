a16e79221ec9b1d4def494ea090fdaf568d1dc82
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-01-11 17:36:21 +0545

    vector with capacity

--- src/array&tuple.rs
+++ src/array&tuple.rs
@@ -1,6 +1,6 @@
 fn main() {
-    // Array
-    let mut array: [u32; 5] = [1, 2, 3, 4, 5]; //Fixed size, same data tyoe
+    // Array:
+    let mut array: [u32; 5] = [1, 2, 3, 4, 5]; //Fixed size, same data type(homogenous)
     array[1] = 100; // can mutate
     dbg!(array[1]); // access
 
@@ -16,7 +16,7 @@ fn main() {
     }
 
     // Tuple -> Fixed size, cannot change
-    let mut tuple = (1, 2.0, 'a', true); // Fixed size, different data type allowed
+    let mut tuple = (1, 2.0, 'a', true); // Fixed size, different data type allowed(heterogenous)
     tuple.0 = 2; // can mutate
     dbg!(tuple.0);
     dbg!(tuple.1);
--- src/vector.rs
+++ src/vector.rs
@@ -1,6 +1,16 @@
 fn main() {
+    // let must list = Vec::<String>::new()
+    // let must list: Vec<String> = Vec::new()
     let mut list: Vec<String> = vec![String::from("Rust"), String::from("Golang")];
-    list.push(String::from("Zig"));
+    list.push(String::from("Zig")); // Always inserts at last
+    list.insert(2, String::from("C++")); // Inserts at given index and shifts all the values in the right by 1 index.
+
+    let val = &list.pop(); // pop returns Option
+    println!("{:?} is boring.", val);
+
+    println!("Get value: {:?}, {:?}", list.get(1), list.get(100)); // .get() will return an Option. This is better than list[100] which will panic since it's out of bound.
+
+    list.remove(2);
 
     for itm in &list {
         println!("{}", itm);
@@ -21,4 +31,16 @@ fn main() {
     // Filter: Filter means to filter out items based on condition. Since, the original vector is Vec<String> it can only return reference items as Vec<&String>. That's why the closure returns &&String
     let filter: Vec<&String> = list.iter().filter(|lan| (*lan).eq("Rust")).collect();
     dbg!(filter);
+}
+
+// Vector Capacity
+fn main() {
+    let mut list = Vec::<u8>::with_capacity(2);
+    list.push(1);
+    list.push(2);
+    println!("{:?} {} {}", list, list.len(), list.capacity());
+
+    // When Rust finds out that the capacity has been filled, it will automatically expand the capacity to a new heap
+    list.push(3);
+    println!("{:?} {} {}", list, list.len(), list.capacity());
 }
