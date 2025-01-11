65255818e576e211c2d1cd26f9c353c7ba6e14ef
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-01-11 23:02:53 +0545

    closures using Box

--- src/closure.rs
+++ src/closure.rs
@@ -1,16 +1,35 @@
 fn main() {
-    let sum = |a, b| a + b;
-    let s = sum(1, 2);
-    println!("Sum = {s}");
-    println!("Sum = {:?}", sum_hoc(1, 2, sum));
+    println!("Sum = {:?}", sum_hoc(1, 2, |a, b| a + b));
+    println!("Subtract = {:?}", sum_hoc(1, 2, |a, b| a - b));
+    println!("Multiply = {:?}", sum_hoc(1, 2, |a, b| a * b));
+    println!("Divide = {:?}", sum_hoc(1, 2, |a, b| a / b));
+
+    // Using heap
+    println!("Box Sum = {:?}", sum_hoc_box(1, 2, Box::new(|a, b| a + b)));
+    println!(
+        "Box Subtract = {:?}",
+        sum_hoc_box(1, 2, Box::new(|a, b| a - b))
+    );
+    println!(
+        "Box Multiply = {:?}",
+        sum_hoc_box(1, 2, Box::new(|a, b| a * b))
+    );
+    println!(
+        "Box Divide = {:?}",
+        sum_hoc_box(1, 2, Box::new(|a, b| a / b))
+    );
 }
 
 fn sum_hoc<T: (Fn(i32, i32) -> i32)>(a: i32, b: i32, cb: T) -> i32 {
     cb(a, b)
 }
 
+fn sum_hoc_box(a: i32, b: i32, cb: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
+    cb(a, b)
+}
+
 // Another way of writing this using where clause.
-fn sum_hoc<T>(a: i32, b: i32, cb: T) -> i32
+fn _sum_hoc<T>(a: i32, b: i32, cb: T) -> i32
 where
     T: Fn(i32, i32) -> i32,
 {
