abcdb5a71c3471a826bec280f7aa21c6d209d184
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-01-19 15:41:22 +0545

    feat: std vs tokio fs

---
+++ src/fs.rs
@@ -0,0 +1,42 @@
+use std::fs;
+use tokio::fs as tokio_fs;
+use tokio::io::{AsyncReadExt, AsyncSeekExt, Result, SeekFrom};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // std fs
+    let read = fs::read("./README.md")?;
+    println!("STD: {:?}", String::from_utf8_lossy(&read));
+
+    // Equivalent to above using Tokio fs
+    let read = tokio_fs::read("./README.md").await?;
+    println!("TOKIO: {:?}", String::from_utf8_lossy(&read));
+
+    // More flexible way to read
+    // NOTE: Rust uses internal cursor to read a file this way.
+    let mut file = tokio_fs::File::open("./README.md").await?;
+
+    // The first 10 bytes is already read. So the cursor advances to 10. You can reset the cursor using using command shown below.
+    let mut buffer = [0; 10];
+    let _ = file.read(&mut buffer).await?;
+    println!("First 10 bytes: {:?}", buffer);
+
+    // Uncomment this to reset the cursor
+    // file.seek(SeekFrom::Start(0)).await?;
+
+    // Since, the cursor starts from 10 and stops at end, your content will be cut off by 10 bytes at first.
+    let mut buffer = Vec::new();
+    let _ = file.read_to_end(&mut buffer).await?;
+    println!("All bytes: {:?}", buffer);
+
+    // Uncomment this to reset the cursor
+    // file.seek(SeekFrom::Start(0)).await?;
+
+    // Since, all content of file has been read, this will return empty.
+    let mut str = String::new();
+    let _ = file.read_to_string(&mut str).await?;
+
+    println!("As String: {:?}", str);
+
+    Ok(())
+}
