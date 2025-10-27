cb771dce2248250cd7acf99bffff48a676abd6e6
Niraj Khatiwada <nirazzz.nk@gmail.com>
2025-10-27 19:40:15 +0545

    vector

---
+++ src/vectors.rs
@@ -0,0 +1,47 @@
+fn vectors() {
+    let mut array: Vec<i32> = Vec::new();
+
+    array.push(1);
+    array.push(2);
+    array.push(3);
+    array.push(4);
+    array.push(-5);
+    array.push(5);
+
+    array.insert(0, 0); // insert at given position
+
+    println!("{:?}", array);
+
+    array.pop();
+    array.remove(4); // Remove at specific position
+
+    println!("{:?}", array);
+    println!("{:?} {:?}", array[0], array.get(0)); // panic vs non-panic access
+
+    array[0] = -1;
+    println!("{:?}", array);
+    println!("{:?} {:?}", &array[0..], &array[1..2]); // slicing
+
+    // find index
+    if let Some(idx) = array.iter().position(|&x| x == -1) {
+        println!("Index is {}", idx)
+    } else {
+        println!("Index is -1",)
+    }
+
+    for (idx, item) in array.iter().enumerate() {
+        println!("{} {}", idx, item)
+    }
+
+    println!("Array {:?}", array);
+    // Map
+    let mapped: Vec<i32> = array.iter().map(|x| x + 1).collect(); // Map dereferences the ref value of .iter() automatically and gives you new vector on .collect()
+    println!("Mapped {:?}", mapped);
+
+    // Filter
+    let filter: Vec<&i32> = array.iter().filter(|x| *x % 2 == 0).collect(); // Filter is just checking for conditions so it'll still refer to the original .iter() value upon .collect() unlike .map()
+    println!("Filtered {:?}", filter);
+
+    array.sort(); //sort occurs in-place
+    println!("Array {:?}", array);
+}
